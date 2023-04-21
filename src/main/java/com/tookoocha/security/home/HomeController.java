418c1a33d44ee4eeac2676510aba5d89fb7970d5
kumarnabin <touchnabin@gmail.com>
2023-04-21 11:06:45 +0545

    controller changed

---
+++ src/main/java/com/tookoocha/security/home/HomeController.java
@@ -0,0 +1,21 @@
+package com.tookoocha.security.home;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Index Message");
+    }
+
+    @GetMapping("/api/v1/home")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Home from secured endpoint");
+    }
+
+}
